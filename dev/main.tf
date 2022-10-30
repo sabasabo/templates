847f8435d934a2279c4c3ec30f40bce83987486a
Liron sabo <lir48290@gmail.com>
2022-10-30 12:41:21 +0200

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -1,7 +1,4 @@
-module "blueprints-private-tf-module" {
-  source = "api-dev.dev.env0.com/4758db1f-1b74-4e76-b217-16257c7cbaad/blueprints-private-tf-module/env0"
-  version = "1.0.1"
-}
+
 
 resource "null_resource" "yoshhdiig" {
 }
