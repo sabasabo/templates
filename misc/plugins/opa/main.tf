47e6367d1845545959da508bbe451f54bbc82aff
lironsabo <lir48290@gmail.com>
2023-01-24 15:18:48 +0200

    wip

--- misc/plugins/opa/main.tf
+++ misc/plugins/opa/main.tf
@@ -1,5 +1,14 @@
-terraform {
-  required_version = ">= 1.2.2"
+data "null_data_source" "values" {
+  inputs = {
+    all_server_ids = concat(
+      aws_instance.green.*.id,
+      aws_instance.blue.*.id,
+    )
+    all_server_ips = concat(
+      aws_instance.green.*.private_ip,
+      aws_instance.blue.*.private_ip,
+    )
+  }
 }
 
 resource "null_resource" "null_resource" {
