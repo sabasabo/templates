13a9c3cd913d15c7cb4b0eb06c5864b675558a76
lironsabo <lir48290@gmail.com>
2023-01-24 14:51:59 +0200

    wip

--- misc/plugins/tflint/main.tf
+++ misc/plugins/tflint/main.tf
@@ -2,11 +2,7 @@ terraform {
   required_version = ">= 1.0.0"
 
   required_providers {
-    aws        = "~> 3.68.0"
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
+    null        = "~> 3.68.0"
   }
 }
 
