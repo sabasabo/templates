761e708c25d1ed43f4e005073f19f9d79103691a
lironsabo <lir48290@gmail.com>
2023-01-24 15:20:00 +0200

    wip

--- misc/plugins/opa/main.tf
+++ misc/plugins/opa/main.tf
@@ -1,14 +1,5 @@
 data "null_data_source" "values" {
-  inputs = {
-    all_server_ids = concat(
-      aws_instance.green.*.id,
-      aws_instance.blue.*.id,
-    )
-    all_server_ips = concat(
-      aws_instance.green.*.private_ip,
-      aws_instance.blue.*.private_ip,
-    )
-  }
+
 }
 
 resource "null_resource" "null_resource" {
